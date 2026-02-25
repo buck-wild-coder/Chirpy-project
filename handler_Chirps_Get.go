87a4a0cd7eb4ac9c66d23ad1b3f0e1a966c75cd7
Snip <185667147+snipwalter@users.noreply.github.com>
2026-02-24 22:58:51 -0500

    H: added get all chirp endpoint

---
+++ handler_Chirps_Get.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.db.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Internal Server Error", err)
+		return
+	}
+	out := make([]Chirp, 0, len(dbChirps))
+	for _, c := range dbChirps {
+		out = append(out, Chirp{
+			ID:        c.ID,
+			CreatedAt: c.CreatedAt,
+			UpdatedAt: c.UpdatedAt,
+			Body:      c.Body,
+			UserID:    c.UserID,
+		})
+	}
+	respondWithJSON(w, http.StatusOK, out)
+}
---
+++ sql/queries/getchirps.sql
@@ -0,0 +1,2 @@
+-- name: GetChirps :many
+SELECT * from chirps ORDER by created_at;
